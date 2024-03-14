a25eb5a4d9a9365c1153d22e2a594ac1a78b515d
rishixyz9 <reach2rishabh@rocketmail.com>
2024-03-14 17:48:48 -0500

    added boilerplate

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-# shdw
-Team management bot
+# SHDW eSports Team Management Discord Bot
 
+## About
 
--Bot should implent gui, adding all commands via a user interfance gui, or the use of context fields
+Discord bot made for SHDW eSports to assist team owners in managing their team, by supporting operations such as dropping, signing, and moving players within a team.
+
+## How to run
+
+- [ ] Use Discord developer portal to create a bot and copy the bot token (if you didn't previously have a bot or a token) and then add the bot to your desired server.
+
+- [ ] Create a `.env` file and put the bot token and other sensitive variables inside of it and should look something like this:
+  
+```bash
+CLIENT_ID=<YOUR_CLIENT_ID>
+GUILD_ID=<YOUR_GUILD_ID>
+DISCORD_TOKEN=<DISCORD_BOT_TOKEN>
+```
+
+- [ ] Run the following commands
+  
+```bash
+python -m venv venv
+source venv/bin/activate # only if you have UNIX/MacOS or are using a WSL shell
+.\venv\Scripts\activate.bat # only if you have windows
+python setup.py install
+python app/bot.py
+```
---
+++ setup.py
@@ -0,0 +1,33 @@
+# python -m venv .
+# pip install -r requirements.txt
+# python -m venv venv
+# source venv/bin/activate
+# .\venv\Scripts\activate.bat
+# pip freeze | Out-File -Encoding UTF8 requirements.txt
+
+from setuptools import setup, find_packages
+
+
+print(open("requirements.txt", encoding="utf-8").read().splitlines())
+
+setup(
+    name="SHDW-Management-Bot",
+    version="0.1",
+    description="Simple team management bot for SHDW eSports",
+    author_email="robonaut5v@gmail.com",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ItzReality/shdw/tree/Management-UI",
+    packages=find_packages(exclude=("tests", "docs")),
+    install_requires=[
+        dep.lstrip("\ufeff")
+        for dep in open("requirements.txt", encoding="utf-8").read().splitlines()
+    ],
+    classifiers=[
+        # Choose your license as you wish
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.12",
+)
